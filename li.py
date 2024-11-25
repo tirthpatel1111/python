b0f2c8af1083895dc273e7f1c540b00750071b79
tirth patel <139980177+tirthpatel1111@users.noreply.github.com>
2024-11-25 17:59:37 +0530

    Add files via upload

---
+++ li.py
@@ -0,0 +1,5 @@
+list=[89,63,412,2,7,41,2]
+print(list[2:3])
+print(list[::3])
+print(list[2::2])
+print(list[::-1])
---
+++ new.py
@@ -0,0 +1,17 @@
+list=[8,1,"tirth",[9,9,6]]
+print(list)
+list.insert(3,"data science")
+print(list)
+list.pop(1)
+print(list)
+list[1]="name"
+print(list)
+li=[]
+li.extend(list)
+print(li)
+li.append("patel")    
+print(li)
+# del li
+# print(li)
+list.remove("data science")
+print(list)
